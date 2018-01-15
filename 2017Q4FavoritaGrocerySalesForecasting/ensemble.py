9066ec42c1f30b2221004e1b555cd7486f88dda6
nasrudin salim <nasdin94@hotmail.com>
2018-01-15 17:31:54 +0800

    favorita sales forecast:simple ensemble, equal weights of 16 models

--- 2017Q4FavoritaGrocerySalesForecasting/ensemble.py
+++ 2017Q4FavoritaGrocerySalesForecasting/ensemble.py
@@ -5,13 +5,31 @@ import pandas as pd
 
 
 
-filelist = ['lgb.csv',
-             '../input/ensemble/lgb.csv',
-             '../input/catboost-starter-lb-0-517/cat1.csv']
+filelist = ['output/lgb.csv',
+             'output/LGBM.csv',
+             'output/aggr.csv',
+	'output/Median-based.csv',
+'output/decisiontree.csv',
+'output/elasticnet.csv',
+'output/extratrees1.csv',
+'output/extratrees2.csv',
+'output/gradientboosting1.csv',
+'output/gradientboosting2.csv',
+'output/gradientboosting3.csv',
+'output/hubermethod.csv',
+'output/linear_model.csv',
+'output/RandomForest01.csv',
+'output/RandomForest02.csv',
+'output/Ridge_method.csv',
+
+
+
+]
 
 outs = [pd.read_csv(f, index_col=0) for f in filelist]
 concat_df = pd.concat(outs, axis=1)
-concat_df.columns = ['1', '2','3']
 
-concat_df["unit_sales"] = (0.2*concat_df['1'] + 0.45*concat_df['2'] + 0.35*concat_df['3'])
+concat_df.columns = [str(x) for x in range(1,len(filelist)+1)]
+print(concat_df.columns)
+concat_df["unit_sales"] = (0.625*concat_df['1'] + 0.625*concat_df['2']+ 0.625*concat_df['3']+ 0.625*concat_df['4']+ 0.625*concat_df['5']+ 0.625*concat_df['6']+ 0.625*concat_df['7']+ 0.625*concat_df['8']+ 0.625*concat_df['9']+ 0.625*concat_df['10']+ 0.625*concat_df['11']+ 0.625*concat_df['12']+ 0.625*concat_df['13']+ 0.625*concat_df['14']+ 0.625*concat_df['15']+0.625*concat_df['16'] )
 concat_df[["unit_sales"]].to_csv("final_ensemble.csv")
